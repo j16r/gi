df36479988304808300a89e25142e581bf60f7ae
John Barker <jebarker@gmail.com>
2014-09-17 17:42:40 -0400

    Evaluate functions like lisp does, built in no-op println

--- vm/src/environment.rs
+++ vm/src/environment.rs
@@ -8,9 +8,13 @@ pub struct Environment {
 
 impl Environment {
   pub fn new() -> Box<Environment> {
+    let mut functions = box HashMap::new();
+
+    functions.insert("println".to_string(), box Nil);
+
     box Environment {
       world: box Cons(box Nil, box Nil),
-      functions: box HashMap::new()
+      functions: functions
     }
   }
 
@@ -22,22 +26,24 @@ impl Environment {
         match *head {
           box Atom(ref value) => {
             println!("Token");
-            self.define_function(head, tail);
+            self.invoke_function(head, tail);
           },
           _ => fail!("Don't know what to do!")
         }
         self.eval(head);
       },
+      box Nil => println!("no-op"),
       _ => fail!("Don't know what to do!")
     }
     box Atom("Hello".to_string())
   }
 
-  fn define_function(&mut self, name: &Box<Token>, args: &Box<Token>) {
+  fn invoke_function(&mut self, name: &Box<Token>, args: &Box<Token>) {
     match *name {
       box Atom(ref value) => {
-        println!("Defined function {:s}", value.to_string());
-        self.functions.insert(value.to_owned(), args.clone());
+        println!("Invoking function {:s}", value.to_string());
+        let ref function = self.functions.get_copy(value);
+        self.eval(function);
       },
       _ => fail!("Tried to create function name with non Atom!")
     }
